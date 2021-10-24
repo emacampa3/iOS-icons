fb2152ef4f80fbe63f2a02d00eef0890caa6920e
Ema Čampa <emacampa3@gmail.com>
2021-10-24 23:50:21 +0200

    finished 8 icons for iPhone

---
+++ script.js
@@ -0,0 +1,15 @@
+const calendarWeek = document.getElementById("calendar-day-week"),
+	calendarDay = document.getElementById("calendar-day")
+
+const clock = () => {
+	let date = new Date()
+
+	let calDay = date.getDate(),
+		calDayweek = date.getDay()
+
+	let calWeek = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
+
+	calendarWeek.innerHTML = `${calWeek[calDayweek]}`
+	calendarDay.innerHTML = calDay
+}
+setInterval(clock, 1000)
